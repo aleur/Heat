ed58d5e6520ebf424757cc87dc99fa15e58a431b
alure <benzstarbusiness@gmail.com>
2024-11-19 22:06:10 -0500

    Added Equip Glasses,Mask,Hat

--- Heat/Heat.cs
+++ Heat/Heat.cs
@@ -8,19 +8,24 @@ using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Security.Policy;
+using System.Windows.Forms;
 
-public class EquipRunningPrimaryWeapon : Script
+public class Heat : Script
 {
     public ScriptSettings Config;
     private List<WeaponHash> weaponList = new List<WeaponHash>();
 
     private int equipDelay = 1000;
     private int lastSprintedTime = 0;
+    private int hatComponent = -1, hatTexture = -1, maskComponent = 0, maskTexture = -1, glassesComponent = -1, glassesTexture = -1;
 
-    public EquipRunningPrimaryWeapon()
+    private Keys equipMaskKey = Keys.Oemcomma, equipHatKey = Keys.OemPeriod, equipGlassesKey = Keys.OemQuestion;
+
+    public Heat()
     {
         Tick += OnTick;
         LoadIniFile("scripts//Heat.ini");
+        KeyUp += OnKeyUp;
     }
 
     public void LoadIniFile(string iniFile)
@@ -33,6 +38,10 @@ public class EquipRunningPrimaryWeapon : Script
         {
             Config = ScriptSettings.Load(iniFile);
             equipDelay = Config.GetValue("SETTINGS", "EquipDelay", equipDelay);
+            equipMaskKey = Config.GetValue("SETTINGS", "EquipMaskKey", equipMaskKey);
+            equipHatKey = Config.GetValue("SETTINGS", "EquipHatKey", equipHatKey);
+            equipGlassesKey = Config.GetValue("SETTINGS", "EquipGlassesKey", equipGlassesKey);
+
             string[] hashes = Config.GetAllValues("HASHES", "HASH");
 
             foreach (var hash in hashes)
@@ -98,7 +107,10 @@ public class EquipRunningPrimaryWeapon : Script
             writer.WriteLine("HASH=1119849093");
             writer.WriteLine("HASH=1834241177");
             writer.WriteLine("[SETTINGS]");
-            writer.WriteLine("EquipDelay=1000");
+            writer.WriteLine($"EquipDelay={equipDelay}");
+            writer.WriteLine($"EquipMaskKey={equipMaskKey}");
+            writer.WriteLine($"EquipHatKey={equipHatKey}");
+            writer.WriteLine($"EquipGlassesKey={equipGlassesKey}");
         }
         UI.Notify("~g~Default configuration created: " + iniFile);
     }
@@ -119,6 +131,70 @@ public class EquipRunningPrimaryWeapon : Script
                     EquipAppropriateWeapon();
                     lastSprintedTime = Game.GameTime;
                 }
+            }
+        }
+    }
+
+    private void OnKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == equipMaskKey)
+        {
+            ToggleMask();
+        }
+        else if (e.KeyCode == equipHatKey)
+        {
+            ToggleHat();
+        }
+        else if (e.KeyCode == equipGlassesKey)
+        {
+            ToggleGlasses();
+        }
+    }
+
+    private void ToggleMask()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            if (GetComponentVariation(playerPed, 1) == 0 && maskComponent != 0)
+            {
+                EquipMask();
+            }
+            else if(GetComponentVariation(playerPed, 1) != 0)
+            {
+                UnequipMask();
+            }
+        }
+    }
+
+    private void ToggleHat()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            if (GetPropVariation(playerPed, 0) == -1 && hatComponent != -1)
+            {
+                EquipHat();
+            }
+            else if (GetPropVariation(playerPed, 0) != -1)
+            {
+                UnequipHat();
+            }
+        }
+    }
+
+    private void ToggleGlasses()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            if (GetPropVariation(playerPed, 1) == -1 && glassesComponent != -1)
+            {
+                EquipGlasses();
+            }
+            else if (GetPropVariation(playerPed, 1) != -1)
+            {
+                UnequipGlasses();
             }
         }
     }
@@ -146,5 +222,147 @@ public class EquipRunningPrimaryWeapon : Script
                 break;
             }
         }
+    }
+    private void UnequipMask()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            string animDict = "missfbi4";
+            string animName = "takeoff_mask";
+
+            if (!Function.Call<bool>(GTA.Native.Hash.HAS_ANIM_DICT_LOADED, animDict))
+            {
+                Function.Call(GTA.Native.Hash.REQUEST_ANIM_DICT, animDict);
+            }
+            Function.Call(GTA.Native.Hash.TASK_PLAY_ANIM, playerPed, animDict, animName, 8f, 8f, 1200, 49, 0, 0, 0, 0);
+            Wait(800);
+
+            maskComponent = GetComponentVariation(playerPed, 1);
+            maskTexture = GetComponentTextureVariation(playerPed, 1);
+
+            SetComponentVariation(playerPed, 1, 0, 0, 0);
+        }
+    }
+    private void EquipMask()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            string animDict = "mp_masks@on_foot";
+            string animName = "put_on_mask";
+
+            if (!Function.Call<bool>(GTA.Native.Hash.HAS_ANIM_DICT_LOADED, animDict))
+            {
+                Function.Call(GTA.Native.Hash.REQUEST_ANIM_DICT, animDict);
+            }
+            Function.Call(GTA.Native.Hash.TASK_PLAY_ANIM, playerPed, animDict, animName, 8f, 8f, 750, 49, 0, 0, 0, 0);
+            Wait(350);
+
+            SetComponentVariation(playerPed, 1, maskComponent, maskTexture, 0);
+        }
+    }
+    private void UnequipHat()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            string animDict = "veh@common@fp_helmet@";
+            string animName = "take_off_helmet_stand";
+
+            if (!Function.Call<bool>(GTA.Native.Hash.HAS_ANIM_DICT_LOADED, animDict))
+            {
+                Function.Call(GTA.Native.Hash.REQUEST_ANIM_DICT, animDict);
+            }
+            Function.Call(GTA.Native.Hash.TASK_PLAY_ANIM, playerPed, animDict, animName, 8f, 8f, 1700, 49, 0, 0, 0, 0);
+            Wait(1200);
+
+            hatComponent = GetPropVariation(playerPed, 0);
+            hatTexture = GetPropTextureVariation(playerPed, 0);
+
+            Function.Call(GTA.Native.Hash.CLEAR_PED_PROP, playerPed, 0);
+        }
+    }
+    private void EquipHat()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            string animDict = "missheistdockssetup1hardhat@";
+            string animName = "put_on_hat";
+
+            if (!Function.Call<bool>(GTA.Native.Hash.HAS_ANIM_DICT_LOADED, animDict))
+            {
+                Function.Call(GTA.Native.Hash.REQUEST_ANIM_DICT, animDict);
+            }
+            Function.Call(GTA.Native.Hash.TASK_PLAY_ANIM, playerPed, animDict, animName, 8f, 8f, 1700, 49, 0, 0, 0, 0);
+            Wait(1700);
+
+            SetPropVariation(playerPed, 0, hatComponent, hatTexture, true);
+        }
+    }
+    private void UnequipGlasses()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            string animDict = "clothingspecs";
+            string animName = "take_off";
+
+            if (!Function.Call<bool>(GTA.Native.Hash.HAS_ANIM_DICT_LOADED, animDict))
+            {
+                Function.Call(GTA.Native.Hash.REQUEST_ANIM_DICT, animDict);
+            }
+            Function.Call(GTA.Native.Hash.TASK_PLAY_ANIM, playerPed, animDict, animName, 8f, 8f, 1500, 49, 0, 0, 0, 0);
+            Wait(1100);
+
+            glassesComponent = GetPropVariation(playerPed, 1);
+            glassesTexture = GetPropTextureVariation(playerPed, 1);
+
+            Function.Call(GTA.Native.Hash.CLEAR_PED_PROP, playerPed, 1);
+        }
+    }
+    private void EquipGlasses()
+    {
+        Ped playerPed = Game.Player.Character;
+        if (playerPed != null)
+        {
+            string animDict = "clothingspecs";
+            string animName = "try_glasses_negative_b";
+
+            if (!Function.Call<bool>(GTA.Native.Hash.HAS_ANIM_DICT_LOADED, animDict))
+            {
+                Function.Call(GTA.Native.Hash.REQUEST_ANIM_DICT, animDict);
+            }
+            Function.Call(GTA.Native.Hash.TASK_PLAY_ANIM, playerPed, animDict, animName, 8f, 8f, 1700, 49, 0, 0, 0, 0);
+            Wait(1200);
+
+            SetPropVariation(playerPed, 1, glassesComponent, glassesTexture, true);
+        }
+    }
+
+    private int GetPropVariation(Ped playerPed, int componentId)
+    {
+        return Function.Call<int>(GTA.Native.Hash.GET_PED_PROP_INDEX, playerPed, componentId);
+    }
+    private int GetPropTextureVariation(Ped playerPed, int componentId)
+    {
+        return Function.Call<int>(GTA.Native.Hash.GET_PED_PROP_TEXTURE_INDEX, playerPed, componentId);
+    }
+    private void SetPropVariation(Ped playerPed, int componentId, int drawableId, int textureId, bool attach)
+    {
+        Function.Call(GTA.Native.Hash.SET_PED_PROP_INDEX, playerPed, componentId, drawableId, textureId, attach);
+    }
+    private int GetComponentVariation(Ped playerPed, int componentId)
+    {
+        return Function.Call<int>(GTA.Native.Hash.GET_PED_DRAWABLE_VARIATION, playerPed, componentId);
+    }
+    private int GetComponentTextureVariation(Ped playerPed, int componentId)
+    {
+        return Function.Call<int>(GTA.Native.Hash.GET_PED_TEXTURE_VARIATION, playerPed, componentId);
+    }
+    private void SetComponentVariation(Ped playerPed, int componentId, int drawableId, int textureId, int paletteId)
+    {
+        Function.Call(GTA.Native.Hash.SET_PED_COMPONENT_VARIATION, playerPed, componentId, drawableId, textureId, paletteId);
     }
 }
